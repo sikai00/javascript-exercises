906aba287639112b064fb9b9c118215ffdde3da5
Si Kai <sikai4111@gmail.com>
2022-05-22 23:53:07 +0800

    Complete foundation 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  if (typeof x === "number" && typeof y === "number") {
+    if (x >= 0 && y >= 0) {
+      if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+      }
 
+      let sum = 0;
+      for (let i = x; i <= y; i++) {
+        sum += i;
+      }
+      return sum;
+    }
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
