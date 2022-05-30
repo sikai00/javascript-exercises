7f7982d42eb8f523eef229d37196499ec52936f5
Si Kai <sikai4111@gmail.com>
2022-05-30 22:45:46 +0800

    Complete foundation 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
+const palindromes = function(str) {
+  const parsed = str
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+    .replace(/\s/g, '')
+    .toLowerCase();
+  const reversed = parsed.split('').reverse().join('');
+  return parsed === reversed;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
