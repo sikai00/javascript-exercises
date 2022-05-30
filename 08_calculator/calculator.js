3d2b1440868767ee688fa23767441bd5d4fe1610
Si Kai <sikai4111@gmail.com>
2022-05-30 21:39:53 +0800

    Complete foundation 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(x, power) {
+	return x ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x <= 0) {
+    return 1;
+  }
+  let sum = 1;
+	while (x > 0) {
+    sum *= x;
+    x -= 1;
+  }
+  return sum;
 };
 
 // Do not edit below this line
