c0648c05883c3a5fca232f00a2b022419178443d
Si Kai <sikai4111@gmail.com>
2022-05-30 23:34:24 +0800

    Complete foundation 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((a, b) => {
+    const currentYear = new Date().getFullYear();
+    const ageA = a.yearOfDeath != undefined 
+      ? a.yearOfDeath - a.yearOfBirth
+      : currentYear - a.yearOfBirth;
+    const ageB = b.yearOfDeath != undefined 
+      ? b.yearOfDeath - b.yearOfBirth
+      : currentYear - b.yearOfBirth;
+    return ageA > ageB ? a : b;
+  });
 };
 
 // Do not edit below this line
